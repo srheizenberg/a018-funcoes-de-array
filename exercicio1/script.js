7a02797cb74057f4af0d78a26c1118f4b1a6992f
srheizenberg <cleitonfreelance@gmail.com>
2022-08-25 00:06:51 -0300

    quase feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,16 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+function capslock(objeto) {
+    const caixalta = {...objeto, nome: objeto.nome.toUpperCase(), profissao: objeto.profissao.toUpperCase(),username: objeto.username.toUpperCase(),senha: objeto.senha.toUpperCase()}
+    return caixalta
+}
+const tudofora = capslock(objeto)
+console.log(tudofora)
+
+function textocorrido(objeto) {
+   
+}
+const tudocorrido = textocorrido(objeto)
+console.log(tudocorrido)
